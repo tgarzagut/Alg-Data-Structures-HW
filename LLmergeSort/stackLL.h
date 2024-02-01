aa66d4b7f45b2ecb84ea8c485867ca73de206a53
tgarzagut <teresa.garzagut@gmail.com>
2024-01-31 23:52:58 -0600

    Stack done (-memory leak)

--- LLmergeSort/stackLL.h
+++ LLmergeSort/stackLL.h
@@ -1,5 +1,4 @@
 
-
 class stackLL
 {
 private:
@@ -7,35 +6,72 @@ private:
 	{
 	public:
 		//put what you need in here
+		int data;
+		node* next;
+
+		node(){
+			data;
+			next = nullptr;
+		}
 	};
 
 	node * top;
 
 public:
 
-	stackLL()
-	{}
+	stackLL() {
+		top = nullptr;
+	}
 
 	//Take care of memory leaks...
-	~stackLL()
-	{}
+	~stackLL(){
+
+	}
 
 	//return true if empty, false if not
-	bool empty()
-	{}
-
+	bool empty() {
+		if(top == nullptr){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 	//add item to top of stack
-	void push(int x)
-	{}
+	void push(int x) {
+		node* temp = new node();
+		temp->data = x;
+		temp->next = top;
+		top = temp;
+	}
 
 	//remove and return top item from stack
-	int pop()
-	{}
+	int pop() {
+		node* temp = top;
+		top = top->next;
+		int popped = temp->data;
+		delete temp;
+		return popped;
+	}
 
 	//add item x to stack, but insert it
 	//right after the current ith item from the top
 	//(and before the i+1 item).
-	void insertAt(int x, int i)
-	{}
+	void insertAt(int x, int i) {
+		node* curr = top;
+		node* temp = new node();
+		temp->data = x;
+		if(i==0){
+			push(x);
+			return;
+		}
+		else{
+			for(int j = 1; j<i; j++){
+				curr = curr->next;
+			}
+		}
+		temp->next = curr->next;
+		curr->next = temp;
+	}
 
 };
