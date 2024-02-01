673b1484dccf12f07c2673466d0f6605aa94db10
tgarzagut <teresa.garzagut@gmail.com>
2024-02-01 00:14:00 -0600

    Queue done (-memory leak)

--- LLmergeSort/queueLL.h
+++ LLmergeSort/queueLL.h
@@ -3,23 +3,66 @@ class queueLL
 {
 private:
 	//put what you need here...
+	class node
+	{
+	public:
+		//put what you need in here
+		int data;
+		node* next;
+
+		node(){
+			data;
+			next = nullptr;
+		}
+	};
+	node* top;
+	node* bottom;
 
 public:
-	queueLL()
-	{}
+	queueLL() {
+		top = nullptr;
+		bottom = nullptr;
+	}
 
-	~queueLL()
-	{}
+	~queueLL() {
+
+	}
 
 	//add item to back of queue
-	void enqueue(int x)
-	{}
+	void enqueue(int x) {
+		node* temp = new node();
+		temp->data = x;
+		temp->next = nullptr;
+		if(top == nullptr){
+			top = temp;
+			bottom = temp;
+		}
+		else{
+			bottom->next = temp;
+			bottom = temp;
+		}
+	}
 
 	//remove and return first item from queue
-	int dequeue()
-	{}
+	int dequeue() {
+		node* temp;
+		temp = top;
+		int value = top->data;
+		top = top->next;
+		delete temp;
+		return value;
+	}
+
+	bool empty() {
+		if(top==nullptr){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 
 	//For the final part of the test program, template this class
 	//and add a decimate method.
 
-}
+};
