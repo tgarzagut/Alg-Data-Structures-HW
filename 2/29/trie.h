7c87f02771efb8e2c753f00f3e74dd33a56fe480
tgarzagut <teresa.garzagut@gmail.com>
2024-03-19 19:44:42 -0500

    redo of files

---
+++ 2/29/trie.h
@@ -0,0 +1,34 @@
+#include <iostream>
+class trie {
+    private:
+        class node{
+            public:
+            bool marked;
+            node* children[256];
+
+            node(){
+                marked = false;
+                for (int i = 0; i < 256; i++){
+                    children[i] = nullptr;
+                }
+            }
+        };
+        node* root;
+    public:
+        trie(){
+        root = new node();
+        }
+        void insert(std::string s){
+            node* arrow = root;
+            for(int i = 0; i < s.size(); i++){
+                if(arrow->children[s[i]] == nullptr){
+                    arrow->children[s[i]] = new node;
+                }
+                arrow = arrow->children[s[i]];
+            }
+            arrow->marked = true;
+        }
+        void printAll(){
+            node* curr = root;
+        }
+};
